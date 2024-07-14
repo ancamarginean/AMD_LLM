cfbc06b86bc5347a97f062c71d6d07063596c31d
chiorana <Anca.Marginean@cs.utcluj.ro>
2024-07-14 22:38:35 +0300

    Question visualisation

--- Questions.py
+++ Questions.py
@@ -16,3 +16,10 @@ st.title("LLMs generated answers for AMD questions")
 
 
 st.write("Choose one of the applications on the left")
+
+st.write("""Experiments published in paper: 
+Muntean, G.A.; Marginean, A.; Groza, A.; Damian, I.; Roman, S.A.; Hapca, M.C.; Sere, A.M.; Mănoiu, R.M.; Muntean, M.V.; Nicoară, S.D. 
+A Qualitative Evaluation of ChatGPT4 and PaLM2’s Response to Patient’s Questions Regarding Age-Related Macular Degeneration. 
+Diagnostics 2024, 14, 1468. 
+https://doi.org/10.3390/diagnostics14141468
+""")
